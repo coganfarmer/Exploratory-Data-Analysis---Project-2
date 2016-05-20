0b045bf4dc94e970a5d6ee5e08b2555e5e7ac18a
coganfarmer <keven@us.ibm.com>
2016-05-20 18:34:19 -0400

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,14 @@
+# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
+# Using the base plotting system, make a plot showing the total PM2.5 emission 
+# from all sources for each of the years 1999, 2002, 2005, and 2008.
+#
+# png("plot1.png", width=number.add.width, height=number.add.height)
+# Group total NEI emissions per year:
+total.emissions <- summarise(group_by(NED, year), Emissions=sum(Emissions))
+clrs <- c("black", "grey", "yellow", "green")
+x1<-barplot(height=total.emissions$Emissions/1000, names.arg=total.emissions$year,
+            xlab="years", ylab=expression('total PM'[2.5]*' emission in kilotons'),ylim=c(0,8000),
+            main=expression('Total PM'[2.5]*' emissions at various years in kilotons'),col=clrs)
+
+# Add text at top of bars
+text(x = x1, y = round(total.emissions$Emissions/1000,2), label = round(total.emissions$Emissions/1000,2), pos = 3, cex = 0.8, col = "black")
